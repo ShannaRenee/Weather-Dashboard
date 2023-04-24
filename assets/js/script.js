e95816c3757aea1ae2b40c54e6b17aebd160c448
Calcifer <shanna.renee.morris@gmail.com>
2023-04-23 20:48:52 -0700

    added forecast functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,9 +14,26 @@ var location = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName.val(
             return response.json()
         })
         .then(function (data) {
-            console.log(data)
+            testFunction(data);
         })
-}
+    }
 
 
+function testFunction(data) {
+    var longitude = data[0].lon;
+    var latitude = data[0].lat;
+    var requestForecast = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude +
+    "&lon=" + longitude + "&appid=f935ee17441f16d1cfe096562d3793e1&units=imperial";
 
+    fetch(requestForecast)
+        .then(function (response) {
+           return response.json()
+        })
+        .then(function (data) {
+            construct(data);
+         })
+    }
+
+function construct(data) {
+
+    }
