831ed98408dcf07ddee4b46e3b7763a680bf8b36
Calcifer <shanna.renee.morris@gmail.com>
2023-04-23 17:52:43 -0700

    added functionality to covert city name to coordinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
-var city = $('#top')
+var cityName = $('#textbox')
+var button = $('button')
 
-city.css
+
+button.on('click', start)
+
+
+function start() {
+var location = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName.val() +
+"&appid=f935ee17441f16d1cfe096562d3793e1"
+
+    fetch(location)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data)
+        })
+}
+
+
+
